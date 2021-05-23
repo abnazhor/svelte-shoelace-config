ff3f43ba2309a3c49c2cca31640630d530a0e169
abnazhor <jorbar982@outlook.com>
2021-05-23 23:59:03 +0200

    My first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { onMount } from 'svelte';
+    import "@shoelace-style/shoelace/dist/themes/base.css";
+
+	onMount(async () => {
+		await import('@shoelace-style/shoelace/dist/components/button/button');
+	});
+</script>
+
+<sl-button>Test</sl-button>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+// svelte.config.js
+import adapter from '@sveltejs/adapter-static';
+
+export default {
+    kit: {
+        adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: null
+        })
+    }
+};
